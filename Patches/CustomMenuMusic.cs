264c15d881f5a6f226caf5651fbfd4beb1ca39fe
Rayd Zain <rzain200@gmail.com>
2024-08-12 20:35:24 -0400

    Combined CustomMenuMusic and SoundtrackMod into one.

---
+++ Patches/CustomMenuMusic.cs
@@ -0,0 +1,102 @@
+﻿using SPT.Reflection.Patching;
+using EFT.UI;
+using System.Collections.Generic;
+using System.IO;
+using System.Reflection;
+using UnityEngine;
+using UnityEngine.Networking;
+using System;
+using HarmonyLib;
+using Comfort.Common;
+using System.Linq;
+using SoundtrackMod;
+
+namespace BobbyRenzobbi.CustomMenuMusic
+{
+    public class CustomMusicPatch : ModulePatch
+    {
+        private static int rndNumber = 0;
+        private static string track = "";
+        private static string trackPath = "";
+        private static List<AudioClip> audioClips = new List<AudioClip>();
+        private static System.Random rand = new System.Random();
+        internal static List<string> menuTrackList = new List<string>();
+        private static List<string> unPlayedTrackList = new List<string>();
+        private static string lastTrack = "";
+
+        protected override MethodBase GetTargetMethod()
+        {
+            return AccessTools.Method(typeof(GUISounds), nameof(GUISounds.method_3));
+        }
+
+        private void LoadNextTrack()
+        {
+            if (menuTrackList == null || menuTrackList.Count == 0)
+            {
+                Logger.LogInfo("No music found in the 'sounds' folder");
+                return;
+            }
+            if (unPlayedTrackList.IsNullOrEmpty())
+            {
+                unPlayedTrackList.AddRange(menuTrackList);
+            }
+            audioClips.Clear();
+
+            do
+            {
+                rndNumber = rand.Next(unPlayedTrackList.Count);
+                track = unPlayedTrackList[rndNumber];
+            }
+            while ((track == lastTrack) && menuTrackList.Count > 1);
+
+            unPlayedTrackList.Remove(track);
+            lastTrack = track;
+            var clip = RequestAudioClip(track);
+            trackPath = Path.GetFileName(track);
+            audioClips.Add(clip);
+            Plugin.LogSource.LogInfo(trackPath + " added to audioClips");
+        }
+
+        private AudioClip RequestAudioClip(string path)
+        {
+            string extension = Path.GetExtension(path);
+            Dictionary<string, AudioType> audioType = new Dictionary<string, AudioType>
+            {
+                [".wav"] = AudioType.WAV,
+                [".ogg"] = AudioType.OGGVORBIS,
+                [".mp3"] = AudioType.MPEG
+            };
+            UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, audioType[extension]);
+            UnityWebRequestAsyncOperation sendWeb = uwr.SendWebRequest();
+
+            while (!sendWeb.isDone)
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Logger.LogError("CustomMenuMusic Mod: Failed To Fetch Audio Clip");
+                return null;
+            }
+            AudioClip audioclip = DownloadHandlerAudioClip.GetContent(uwr);
+            return audioclip;
+        }
+
+        [PatchPrefix]
+        static bool Prefix()
+        {
+            CustomMusicPatch patch = new CustomMusicPatch();
+            patch.LoadNextTrack();
+            //Credit to SamSWAT for discovering that the game loads infinitely if the audioClip_0 array has only one element
+            if (audioClips.Count == 0)
+            {
+                return true;
+            }
+            if (audioClips.Count == 1)
+            {
+                audioClips.Add(audioClips[0]);
+            }
+            Traverse.Create(Singleton<GUISounds>.Instance).Field("audioClip_0").SetValue(audioClips.ToArray());
+            Plugin.LogSource.LogInfo("Playing " + trackPath);
+            return true;
+        }
+    }
+}
---
+++ Plugin.cs
@@ -0,0 +1,149 @@
+﻿using BepInEx;
+using BepInEx.Logging;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using UnityEngine;
+using System.IO;
+using UnityEngine.Networking;
+using BepInEx.Configuration;
+using EFT;
+using Comfort.Common;
+using BobbyRenzobbi.CustomMenuMusic;
+
+namespace SoundtrackMod
+{
+    [RequireComponent(typeof(AudioSource))]
+    public class Audio : MonoBehaviour
+    {
+        public static AudioSource myaudioSource;
+        public static void SetClip(AudioClip clip)
+        {
+            myaudioSource.clip = clip;
+        }
+        public static void AdjustVolume(float volume)
+        {
+            myaudioSource.volume = volume;
+        }
+    }
+    [BepInPlugin("BobbyRenzobbi.MusicPlayer", "BobbysMusicPlayer", "0.0.1")]
+    public class Plugin : BaseUnityPlugin
+    {
+        private static AudioClip unityAudioClip;
+        private static System.Random rand = new System.Random();
+        private async void RequestAudioClip(string path)
+        {
+            string extension = Path.GetExtension(path);
+            Dictionary<string, AudioType> audioType = new Dictionary<string, AudioType>
+            {
+                [".wav"] = AudioType.WAV,
+                [".ogg"] = AudioType.OGGVORBIS,
+                [".mp3"] = AudioType.MPEG
+            };
+            UnityWebRequest uwr = UnityWebRequestMultimedia.GetAudioClip(path, audioType[extension]);
+            UnityWebRequestAsyncOperation sendWeb = uwr.SendWebRequest();
+
+            while (!sendWeb.isDone)
+                await Task.Yield();
+
+            if (uwr.isNetworkError || uwr.isHttpError)
+            {
+                Logger.LogError("Soundtrack Mod: Failed To Fetch Audio Clip");
+                return;
+            }
+            else
+            {
+                unityAudioClip = DownloadHandlerAudioClip.GetContent(uwr);
+                return;
+            }
+        }
+
+        private static Dictionary<string, AudioClip> tracks = new Dictionary<string, AudioClip>();
+        internal static ManualLogSource LogSource;
+        private static string track = "";
+        private static string trackPath = "";
+        private static List<string> trackList = new List<string>();
+        private static List<string> unPlayedTrackList = new List<string>();
+        private static string lastTrack = "";
+        private static int rndNumber = 0;
+        public static ConfigEntry<float> MusicVolume { get; set; }
+        private void LoadNextTrack()
+        {
+            if (unPlayedTrackList.IsNullOrEmpty())
+            {
+                unPlayedTrackList.AddRange(trackList);
+            }
+            tracks.Clear();
+
+            do
+            {
+                rndNumber = rand.Next(unPlayedTrackList.Count);
+                track = unPlayedTrackList[rndNumber];
+            } 
+            while ((track == lastTrack) && trackList.Count > 1);
+
+            unPlayedTrackList.Remove(track);
+            lastTrack = track;
+            trackPath = Path.GetFileName(track);
+            RequestAudioClip(track);
+            LogSource.LogInfo("loaded " + trackPath);
+            tracks[trackPath] = unityAudioClip;
+        }
+
+        private void Awake()
+        {
+            CustomMusicPatch.menuTrackList.AddRange(Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\CustomMenuMusic\\sounds"));
+            trackList.AddRange(Directory.GetFiles(AppDomain.CurrentDomain.BaseDirectory + "\\BepInEx\\plugins\\Soundtrack\\sounds"));
+            string settings = "Soundtrack Settings";
+            MusicVolume = Config.Bind<float>(settings, "In-raid music volume", 0.025f, new ConfigDescription("Volume of the music heard in raid", new AcceptableValueRange<float>(0f, 1f)));
+            LogSource = Logger;
+            LogSource.LogInfo("plugin loaded!");
+            new CustomMusicPatch().Enable();
+        }
+
+        private void LateUpdate()
+        {
+            if (Audio.myaudioSource != null)
+            {
+                try
+                {
+                    Audio.AdjustVolume(MusicVolume.Value);
+                }
+                catch (Exception exception)
+                {
+                    LogSource.LogError(exception);
+                }
+            }
+            if (trackList.IsNullOrEmpty())
+            {
+                return;
+            }
+            if (Singleton<GameWorld>.Instance == null || (Singleton<GameWorld>.Instance?.MainPlayer is HideoutPlayer))
+            {
+                return;
+            }
+            if (Audio.myaudioSource == null)
+            {
+                try
+                {
+                    Audio.myaudioSource = gameObject.GetOrAddComponent<AudioSource>();
+                }
+                catch (Exception ex)
+                {
+                    LogSource.LogInfo(ex.Message);
+                }
+            }
+            if (Singleton<GameWorld>.Instance.MainPlayer == null)
+            {
+                return;
+            }
+            if (!Audio.myaudioSource.isPlaying)
+            {
+                LoadNextTrack();
+                Audio.SetClip(tracks[trackPath]);
+                Audio.myaudioSource.Play();
+            }
+        }
+    }
+}
